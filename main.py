758fc75c9d24dffa73e3c581c2688ea8f7673529
Ben <benjaminburton2020@outlook.com>
2026-09-03 21:21:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 import webbrowser
 
 webbrowser.open('localhost:8080')
-#MUZBQWotQXd4ZTBUY0JVdXlUVjJGNHA3MVhxdktTa2FDMzhUM0R2VkxzQ2MvZWRpdD91c3A9c2hhcmluZw==
+#MUNOQi1PY1NLdEZBUFlsakVEWHM0UTJHU1I3blYxZU9vM1ppcmVOOHJINEEvZWRpdD91c3A9c2hhcmluZw==
